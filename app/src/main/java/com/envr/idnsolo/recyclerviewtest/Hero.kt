c7f1cd2b5ff7f023bcd5ea58bf3c2968e95a98da
zlibdev <enver.avisena@gmail.com>
2023-11-10 21:13:23 +0700

    New

---
+++ app/src/main/java/com/envr/idnsolo/recyclerviewtest/Hero.kt
@@ -0,0 +1,12 @@
+package com.envr.idnsolo.recyclerviewtest
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+
+data class Hero(
+    val name: String,
+    val description: String,
+    val photo: Int
+) : Parcelable
